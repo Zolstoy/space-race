2ecb1738dc1b59a9321e54ba618b16f24e48b98c
Zolstoy <zolstoy.dev@gmail.com>
2025-08-18 11:42:32 +0200

    WIP

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,52 +2,76 @@ use tokio::net::{TcpListener, TcpStream};
 use tungstenite::protocol::Message;
 use futures::{SinkExt, StreamExt};
 use tokio_tungstenite::tungstenite;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Player {
+    position: (f32, f32, f32),
+    velocity: (f32, f32, f32),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Others{
+    position: (f32, f32, f32),
+    velocity: (f32, f32, f32),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum GameState {
+    PlayerState(Player),
+    OthersState(Others),
+}
 
 #[tokio::main]
 async fn main() {
     let addr = "127.0.0.1:8080";
     let listener = TcpListener::bind(addr).await.expect(format!("Failed to bind to {}", addr).as_str());
 
-    println!("Server started on {}", addr);
+    log::info!("Server started on {}", addr);
 
     while let Ok((stream, _)) = listener.accept().await {
         tokio::spawn(handle_connection(stream));
     }
 
-    println!("Server ended gracefully");
+    log::info!("Server ended gracefully");
 }
 
 async fn handle_connection(stream: TcpStream) {
     let addr = stream.peer_addr().expect("Invalid address");
-    println!("New connection from {}", addr);
+    log::info!("New connection from {}", addr);
 
     let mut ws_stream = tokio_tungstenite::accept_async(stream)
         .await
         .expect("Failed to upgrade to WebSocket");
 
-    // Send a welcome message
+    let player = Player {
+        position: (0.0, 0.0, 0.0),
+        velocity: (0.0, 0.0, 0.0),
+    };
+
+    let player_state_json = serde_json::to_string(&GameState::PlayerState(player))
+        .expect("Failed to serialize player state");
+
     ws_stream
-        .send(Message::Text("Hello!".into()))
+        .send(Message::Text(player_state_json.into()))
         .await
         .expect("Failed to send message");
 
-    // Listen for messages from the client
     while let Some(msg) = ws_stream.next().await {
         match msg {
             Ok(Message::Text(text)) => {
-                println!("Received from {}: {}", addr, text);
-                // RRespond with an echo
+                log::info!("Received from {}: {}", addr, text);
                 ws_stream
                     .send(Message::Text(format!("You said: {}", text).into()))
                     .await
                     .expect("Failed to send message");
             }
             Ok(Message::Close(_)) => {
-                println!("Client {} disconnected", addr);
+                log::info!("Client {} disconnected", addr);
                 break;
             }
             Err(e) => {
-                println!("Error with {}: {}", addr, e);
+                log::info!("Error with {}: {}", addr, e);
                 break;
             }
             _ => {}
